4678dee1c34e1b4d396e3de5504032131bdd6052
elib00 <joshuanapinas19@gmail.com>
2024-10-01 05:53:39 +0800

    [Joshua] Added models for Cinema app

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import CustomUser
+from movies.models import Movie
 
-# Create your models here.
+class Cinema(models.Model):
+    capacity = models.PositiveIntegerField(default=0)
+    cinema_number = models.PositiveIntegerField()
+    
+class ScheduledMovie(models.Model):
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE, related_name="cinema_scheduled_movies")
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="scheduled_movies")
+    audience_number = models.PositiveIntegerField(default=0)
+    schedule = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+    
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profile_tickets")
+    scheduled_movie = models.ForeignKey(ScheduledMovie, on_delete=models.CASCADE, related_name="movie_tickets")
+    seat_identifier = models.CharField(max_length=3, default="")
+    is_active = models.BooleanField(default=True)
