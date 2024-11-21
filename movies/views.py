973cc05d15d75ce9e3ddd75ad535b99a4b6a3705
SummerIshi <summerrodrigo07@gmail.com>
2024-11-21 14:25:33 +0800

    can view available scheduled date and time

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from .models import Movie
+from cinema.models import ScheduledMovie
 
 
 def movie_list(request):
@@ -18,6 +19,18 @@ def movie_details(request, movie_id):
 def movie_book(request, movie_id):
     try:
         movie = Movie.objects.get(id=movie_id)
-        return render(request, 'movie_book.html', {'movie': movie})
+        scheduled_movies = ScheduledMovie.objects.filter(movie_id=movie_id, is_active=True)
+        
+        cinemas = {sm.cinema for sm in scheduled_movies}
+        dates = {sm.schedule.date() for sm in scheduled_movies}
+        
+        context = {
+        'movie': movie,
+        'scheduled_movies': scheduled_movies,
+        'cinemas': cinemas,
+        'dates': sorted(dates),  
+        }
+
+        return render(request, 'movie_book.html', context)
     except Movie.DoesNotExist:
         return HttpResponse("Movie not found")
