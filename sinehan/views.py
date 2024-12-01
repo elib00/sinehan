2ce15a953222c015f676c1e6f3168fb01aec0f8e
SummerIshi <summerrodrigo07@gmail.com>
2024-12-01 22:55:48 +0800

    Added dynamic home page

--- sinehan/views.py
+++ sinehan/views.py
@@ -2,12 +2,16 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from movies.models import Movie
 from movies.views import movie_list
+from django.core.serializers import serialize
 
 def movies_view(request):
     return movie_list(request)
 
 def home_view(request):
-    return render(request, 'home.html')
+    movies = Movie.objects.all()
+    movies_json = serialize('json', movies)
+    
+    return render(request, 'home.html', {'movies': movies, 'movies_json': movies_json})
 
 def coming_view(request):
     return render(request, 'coming.html')
